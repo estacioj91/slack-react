36c4d02f68fc9bab8599c32c49c842f252e51555
John Estacio <john.estacio@workato.com>
2022-08-01 16:51:45 -0700

    load screne added

--- src/App.js
+++ src/App.js
@@ -12,9 +12,25 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
 import Login from './components/Login';
+import Spinner from "react-spinkit";
 function App() {
   const [user, loading]= useAuthState(auth);
 
+  if(loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContainer>
+          <img src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg" alt="slack" />
+
+          <Spinner
+            name="ball-spin-fade-loader"
+            fadeIn="none"
+            color="purple"
+          />
+        </AppLoadingContainer>
+      </AppLoading>
+    )
+  }
   return (
     <div className="app">
       <Router>
@@ -42,3 +58,24 @@ const AppBody = styled.div`
   display: flex;
   height: 100vh;
 `;
+
+const AppLoading = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  width: 100%;
+`
+const AppLoadingContainer = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  >img {
+    height: 100px;
+    padding: 20px;
+    margin-bottom: 40px;
+  }
+`
