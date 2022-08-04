ccacfdea2b43cd711523ad949cd3c11744062881
John Estacio <john.estacio@workato.com>
2022-08-04 16:46:08 -0700

    fix

--- src/components/SidebarOption.js
+++ src/components/SidebarOption.js
@@ -30,7 +30,7 @@ const SidebarOption = ({Icon, title, addChannelOption, id}) => {
     }
     useEffect(() => {
         channel.current?.addEventListener('click', function(e){
-            for(let i = 0; i < e.path.length; i++){
+            for(let i = 0; i < e.composedPath(); i++){
                 if(e.composedPath()[i].id){
                     selectChannel(channel);
                     break;
